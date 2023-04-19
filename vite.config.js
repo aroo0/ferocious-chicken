87ee25a316f298994c51a1c3bd635e1d197c2299
aroogit commit -m First <arek.dec@gmail.com>
2023-04-19 19:45:14 +0200

    2th

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import Icons from 'unplugin-icons/vite'
+
+
+export default defineConfig({
+    plugins: [
+        Icons({
+            autoInstall: true,
+          })],
+  })
