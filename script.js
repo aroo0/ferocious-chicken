0e54d2a5cee6d6bed2c3c1f7f1b702f85fef1f04
aroogit commit -m First <arek.dec@gmail.com>
2023-04-23 20:22:07 +0200

    added nav active links functionality

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const svgElement = document.querySelector('.responsive-svg');
 const heroElement = document.querySelector('.hero');
 const yearContainer = document.querySelector('#year-container')
 
-
 // Update the height of the hero element based on the SVG's current height
 function updateHeroHeight() {
   const svgHeight = svgElement.getBoundingClientRect().height;
@@ -15,7 +14,6 @@ updateHeroHeight();
 window.addEventListener('resize', updateHeroHeight);
 
 
-
 // Insert current year into footer
 
 function generateYear() {
@@ -25,6 +23,41 @@ function generateYear() {
   yearContainer.textContent = year
 
 }
-
-
 generateYear()
+
+// Navlink menu active class on scroll
+
+const sectionElements = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link")
+let currentSection = 'home'
+
+
+
+// Debounce function to limit the frequency of event handler invocation
+const debounce = (func, delay) => {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(func, delay);
+  };
+};
+
+const handleScroll = () => {
+
+  sectionElements.forEach(sectionElement => {
+    if (window.scrollY >= (sectionElement.offsetTop - (sectionElement.clientHeight / 10) )) {
+      currentSection = sectionElement.id
+    }
+  })
+
+  navLinks.forEach(navLinkElement => {
+    if (navLinkElement.href.includes(currentSection)) {
+      document.querySelector('.active')?.classList.remove('active')
+      navLinkElement.classList.add('active')
+    }
+  })
+}
+
+
+// Add event listener with debounced event handler
+window.addEventListener('scroll', debounce(handleScroll, 100)); // Adjust delay as needed
