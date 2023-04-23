a9f3837efcf0c3b62a5dde822bb1d3d4bdbe7b62
aroogit commit -m First <arek.dec@gmail.com>
2023-04-23 18:53:16 +0200

    added projects and contact pages

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Get the SVG element and the hero element
 const svgElement = document.querySelector('.responsive-svg');
 const heroElement = document.querySelector('.hero');
-
+const yearContainer = document.querySelector('#year-container')
 
 
 // Update the height of the hero element based on the SVG's current height
@@ -13,3 +13,18 @@ function updateHeroHeight() {
 // Call the updateHeroHeight function initially and on window resize
 updateHeroHeight();
 window.addEventListener('resize', updateHeroHeight);
+
+
+
+// Insert current year into footer
+
+function generateYear() {
+
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  yearContainer.textContent = year
+
+}
+
+
+generateYear()
