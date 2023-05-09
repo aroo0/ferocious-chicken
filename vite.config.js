1d856bfe2291c287640a3e2c382b373f7db9977b
aroogit commit -m First <arek.dec@gmail.com>
2023-05-09 20:20:03 +0200

    changed dir of index in config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import { resolve } from 'path'
 
 
 export default defineConfig({
+  root: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       input: {
+        index: resolve(__dirname, 'index.html'),
         success: resolve(__dirname, 'form/success.html')
       }
     }
   }
   })
-
-  
